b5c67ed66ea556d866a3973489ae8b7fb4f00eb0
Weijun <weijun.huang22@imperial.ac.uk>
2023-12-13 20:59:39 +0000

    modified version of blur using thread pool

--- Makefile
+++ Makefile
@@ -1,22 +1,25 @@
 all: picture_lib concurrent_picture_lib blur_opt_exprmt picture_compare
 
-picture_lib: SeqMain.o Utils.o Picture.o PicProcess.o
-	gcc sod_118/sod.c SeqMain.o Utils.o Picture.o PicProcess.o -I sod_118 -lm -o picture_lib
+picture_lib: SeqMain.o Utils.o Picture.o PicProcess.o thpool.o
+	gcc sod_118/sod.c SeqMain.o Utils.o Picture.o PicProcess.o thpool.o -I sod_118 -lm -o picture_lib
 
-concurrent_picture_lib: ConcMain.o Utils.o Picture.o PicProcess.o PicStore.o
-	gcc sod_118/sod.c ConcMain.o Utils.o Picture.o PicProcess.o PicStore.o -I sod_118 -lm -lpthread -o concurrent_picture_lib	
+concurrent_picture_lib: ConcMain.o Utils.o Picture.o PicProcess.o PicStore.o thpool.o
+	gcc sod_118/sod.c ConcMain.o Utils.o Picture.o PicProcess.o PicStore.o thpool.o -I sod_118 -lm -lpthread -o concurrent_picture_lib
 
-blur_opt_exprmt: BlurExprmt.o Utils.o Picture.o PicProcess.o
-	gcc sod_118/sod.c BlurExprmt.o Utils.o Picture.o PicProcess.o -I sod_118 -lm -lpthread -o blur_opt_exprmt
+blur_opt_exprmt: BlurExprmt.o Utils.o Picture.o thpool.o
+	gcc sod_118/sod.c BlurExprmt.o Utils.o Picture.o thpool.o -I sod_118 -lm -lpthread -o blur_opt_exprmt
 
 picture_compare: Compare.o Utils.o Picture.o
 	gcc sod_118/sod.c Compare.o Utils.o Picture.o -I sod_118 -lm -o picture_compare
+
+
+thpool.o: thpool.c thpool.h
 
 Utils.o: Utils.h Utils.c
 
 Picture.o: Utils.h Picture.h Picture.c
 
-PicProcess.o: Utils.h Picture.h PicProcess.h PicProcess.c
+PicProcess.o: Utils.h Picture.h PicProcess.h PicProcess.c thpool.h
 
 SeqMain.o: SeqMain.c Utils.h Picture.h PicProcess.h
 
--- PicProcess.c
+++ PicProcess.c
@@ -1,6 +1,7 @@
 #include "PicProcess.h"
 #include <pthread.h>
 #include <unistd.h>
+#include "thpool.h"
 
 #define NO_RGB_COMPONENTS 3
 #define BLUR_REGION_SIZE 9
@@ -140,81 +141,13 @@ void flip_picture(struct picture *pic, char plane)
   overwrite_picture(pic, &tmp);
 }
 
-void blur_picture(struct picture *pic)
+void calculate_new_blur_pixel(int i, int j, struct picture *input, struct picture *output)
 {
-  // make new temporary picture to work in
-  struct picture tmp;
-  init_picture_from_size(&tmp, pic->width, pic->height);
-
-  // iterate over each pixel in the picture
-  for (int i = 0; i < tmp.width; i++)
-  {
-    for (int j = 0; j < tmp.height; j++)
-    {
-
-      // set-up a local pixel on the stack
-      struct pixel rgb = get_pixel(pic, i, j);
-
-      // don't need to modify boundary pixels
-      if (i != 0 && j != 0 && i != tmp.width - 1 && j != tmp.height - 1)
-      {
-
-        // set up running RGB component totals for pixel region
-        int sum_red = rgb.red;
-        int sum_green = rgb.green;
-        int sum_blue = rgb.blue;
-
-        // check the surrounding pixel region
-        for (int n = -1; n <= 1; n++)
-        {
-          for (int m = -1; m <= 1; m++)
-          {
-            if (n != 0 || m != 0)
-            {
-              rgb = get_pixel(pic, i + n, j + m);
-              sum_red += rgb.red;
-              sum_green += rgb.green;
-              sum_blue += rgb.blue;
-            }
-          }
-        }
-
-        // compute average pixel RGB value
-        rgb.red = sum_red / BLUR_REGION_SIZE;
-        rgb.green = sum_green / BLUR_REGION_SIZE;
-        rgb.blue = sum_blue / BLUR_REGION_SIZE;
-      }
-
-      // set pixel to computed region RBG value (unmodified if boundary)
-      set_pixel(&tmp, i, j, &rgb);
-    }
-  }
-
-  // clean-up the old picture and replace with new picture
-  clear_picture(pic);
-  overwrite_picture(pic, &tmp);
-}
-
-pthread_mutex_t lock;
-// record the number of threads running
-int count = 0;
-void *help_blur(struct task_args *args)
-{
-
-  int i = args->i;
-  int j = args->j;
-  struct picture *input = args->input;
-  struct picture *output = args->output;
-  pthread_mutex_lock(&lock);
-  int width = input->width;
-  int height = input->height;
   // set-up a local pixel on the stack
   struct pixel rgb = get_pixel(input, i, j);
-  count++;
-  pthread_mutex_unlock(&lock);
 
   // don't need to modify boundary pixels
-  if (i != 0 && j != 0 && i != width - 1 && j != height - 1)
+  if (i != 0 && j != 0 && i != output->width - 1 && j != output->height - 1)
   {
 
     // set up running RGB component totals for pixel region
@@ -229,9 +162,7 @@ void *help_blur(struct task_args *args)
       {
         if (n != 0 || m != 0)
         {
-          pthread_mutex_lock(&lock);
           rgb = get_pixel(input, i + n, j + m);
-          pthread_mutex_unlock(&lock);
           sum_red += rgb.red;
           sum_green += rgb.green;
           sum_blue += rgb.blue;
@@ -246,14 +177,45 @@ void *help_blur(struct task_args *args)
   }
 
   // set pixel to computed region RBG value (unmodified if boundary)
-  pthread_mutex_lock(&lock);
   set_pixel(output, i, j, &rgb);
-  count--;
-  pthread_mutex_unlock(&lock);
-  free(args);
-  pthread_exit(NULL);
 }
 
+// sequential verison
+void blur_picture(struct picture *pic)
+{
+  // make new temporary picture to work in
+  struct picture tmp;
+  init_picture_from_size(&tmp, pic->width, pic->height);
+
+  // iterate over each pixel in the picture
+  for (int i = 0; i < tmp.width; i++)
+  {
+    for (int j = 0; j < tmp.height; j++)
+    {
+      calculate_new_blur_pixel(i, j, pic, &tmp);
+    }
+  }
+
+  // clean-up the old picture and replace with new picture
+  clear_picture(pic);
+  overwrite_picture(pic, &tmp);
+}
+
+// helper function runs by child for parallel blur
+void *help_parallel_blur(struct task_args *args)
+{
+  int i = args->i;
+  int j = args->j;
+  struct picture *input = args->input;
+  struct picture *output = args->output;
+
+  calculate_new_blur_pixel(i, j, input, output);
+
+  free(args);
+  return NULL;
+}
+
+// parallel version
 void parallel_blur_picture(struct picture *pic)
 {
   // make new temporary picture to work in
@@ -261,45 +223,23 @@ void parallel_blur_picture(struct picture *pic)
   init_picture_from_size(&tmp, pic->width, pic->height);
   int height = tmp.height;
   int width = tmp.width;
-  int num_threads = width * height;
-  pthread_t threads[num_threads];
   // iterate over each pixel in the picture
-  printf("creating threads\n");
-  pthread_mutex_init(&lock, NULL);
-  int pre = 0;
-  for (int k = 0; k < num_threads; k++)
+  threadpool thpool = thpool_init(16);
+
+  for (int i = 0; i < width; i++)
   {
-    // waiting for threads to finish
-    if (k % THREAD_NUM == 0)
+    for (int j = 0; j < height; j++)
     {
-      pthread_mutex_unlock(&lock);
-      // 0.5s
-      // usleep(500000);
-      for (int i = pre; i < k; i++)
-      {
-        pthread_join(threads[i], NULL);
-      }
-      pre = k;
+      struct task_args *args = malloc(sizeof(struct task_args));
+      args->i = i;
+      args->j = j;
+      args->input = pic;
+      args->output = &tmp;
+      thpool_add_work(thpool, (void (*)(void *))help_parallel_blur, args);
     }
-    // lock will only release when waiting for threads
-    pthread_mutex_trylock(&lock);
-    struct task_args *args = malloc(sizeof(struct task_args));
-    args->i = k / height;
-    args->j = k % height;
-    args->input = pic;
-    args->output = &tmp;
-    pthread_create(&threads[k], NULL, (void *(*)(void *))help_blur, args);
-    //    pthread_mutex_unlock(&lock);
   }
-  pthread_mutex_unlock(&lock);
-  //  printf("waiting for threads to finish\n");
-  for (int i = pre; i < num_threads; i++)
-  {
-    pthread_join(threads[i], NULL);
-  }
-  //  printf("done\n");
-  // clean-up the old picture and replace with new picture
+  thpool_wait(thpool);
+  thpool_destroy(thpool);
   clear_picture(pic);
   overwrite_picture(pic, &tmp);
-  pthread_mutex_destroy(&lock);
 }
